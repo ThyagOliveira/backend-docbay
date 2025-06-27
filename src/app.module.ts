f5e45e06a9bd92d028384985f0df35b0d35254dc
ThyagOliveira <taothyago@gmail.com>
2025-06-27 01:51:38 +0100

    Add prisma schema and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GithubModule } from './github/github.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), HttpModule, GithubModule],
   controllers: [AppController],
   providers: [AppService],
 })
