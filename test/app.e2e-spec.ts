4691607d68b5b308499eddc1a8ce719b645839b6
ThyagOliveira <taothyago@gmail.com>
2025-06-27 02:40:00 +0100

    Add tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
@@ -13,7 +13,20 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
@@ -21,5 +34,64 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('POST /github/thyagoliveira - should creat or update github user', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/github/thyagoliveira')
+      .expect(201);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.githubId).toBeDefined();
+    expect(response.body.languages).toBeInstanceOf(Array);
+  });
+
+  it('POST /github/thyagoliveiraFakeUser - should return 404', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/github/thyagoliveiraFakeUser')
+      .expect(404);
+
+    expect(response.body).toHaveProperty('statusCode', 404);
+    expect(response.body.message).toMatch(/not found/i);
+  });
+
+  it('GET /github - should return users list', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/github')
+      .expect(200);
+
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('GET /github?language=TypeScript - should apply filter by language', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/github?language=TypeScript')
+      .expect(200);
+
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('GET /github?location=Porto - should apply filter by location', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/github?location=Porto')
+      .expect(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('GET /github?age=10 - should return 400 (unknown query param)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/github?age=10')
+      .expect(400);
+
+    expect(response.body).toHaveProperty('statusCode', 400);
+    expect(response.body.message).toContain('property age should not exist');
+  });
+
+  it('GET /github?language=JavaScript&xpto=abc - should return 400 (unknown param xpto)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/github?language=JavaScript&xpto=abc')
+      .expect(400);
+
+    expect(response.body.message).toContain('property xpto should not exist');
   });
 });
